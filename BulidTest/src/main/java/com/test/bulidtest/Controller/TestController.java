4661b301189e84356a054f121131eb24fd969e8e
Seogi <mls0000@naver.com>
2022-12-30 19:40:39 +0900

    빌드테스트

---
+++ BulidTest/src/main/java/com/test/bulidtest/Controller/TestController.java
@@ -0,0 +1,15 @@
+package com.test.bulidtest.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController {
+
+    @RequestMapping(value = "/")
+    public String home(){
+
+        return "index.html";
+    }
+}
